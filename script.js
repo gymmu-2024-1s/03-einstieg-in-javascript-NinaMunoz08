0f5c3898ed25e3fea11a51c96d29c1973115c90e
NinaMunoz08 <e227298@edu.sbl.ch>
2024-11-01 13:38:53 +0000

    aufgabe 10 erstellt

--- script.js
+++ script.js
@@ -144,3 +144,12 @@ export function aufgabe09(args) {
   return is6long
 }
 linkupExerciseHandler("[data-click=aufgabe09]", aufgabe09)
+
+export function aufgabe10(args) {
+  const input = args
+  const result = []
+
+  return result.join("")
+}
+
+linkupExerciseHandler("[data-click=aufgabe010]", aufgabe10)
